47b97b2ab1362298858740d27b109bec10221859
Loic Raucy <loic.raucy@datadoghq.com>
2024-09-18 16:52:31 +0200

    Add comment.

--- src/main.go
+++ src/main.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("this is a very long line", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long")
 	fmt.Println("vim-go", 1)
+	// test
 }
