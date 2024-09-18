5f45b845d598cacc611999959bcda7f4b18a016e
Loic Raucy <loic.raucy@datadoghq.com>
2024-09-18 15:57:09 +0200

    Add another line.

--- src/main.go
+++ src/main.go
@@ -4,5 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("this is a very long line", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long")
+	fmt.Println("another", "one long", "one long", "one long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long", "very long")
 	fmt.Println("vim-go", 1)
 }
